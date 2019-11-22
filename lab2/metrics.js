22f5a26cd101c4aecb90a34a661e7e37dae81da7
Billette <maxime.billette@gmail.com>
2019-11-22 11:45:23 +0100

    lab2 still in dev....

---
+++ lab2/metrics.js
@@ -0,0 +1,8 @@
+module.exports = {
+    get: (callback) => {
+      callback(null, [
+        { timestamp: new Date('2013-11-04 14:00 UTC').getTime(), value:12}
+      , { timestamp: new Date('2013-11-04 14:30 UTC').getTime(), value:15}
+      ])
+    }
+}
