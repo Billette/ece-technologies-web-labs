16752eb1fc0305f26c4433d2f15f90028fb6aa08
Sarah Lehuby <sarah.lehuby@bbox.fr>
2019-11-22 23:41:53 +0100

    Lab2_typescript ajout des fichiers metrics
    bug à fixer : css et jquery non supportés

--- lab2/lab2_typescript/src/server.ts
+++ lab2/lab2_typescript/src/server.ts
@@ -1,11 +1,33 @@
 import express = require('express')
+import { MetricsHandler } from './metrics'
 
 const app = express()
 const port: string = process.env.PORT || '8080'
 
+import path = require('path')
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.set('views', __dirname + "/view")
+app.set('view engine', 'ejs');
+
 app.get('/', (req: any, res: any) => {
-  res.write('Hello world')
+  res.write('Re Bonjour !')
   res.end()
+})
+
+app.get(
+    '/hello/:name', 
+    (req : any, res: any) => {
+        res.render('hello.ejs', {name: req.params.name})
+})
+
+app.get('/metrics.json', (req: any, res: any) => {
+  MetricsHandler.get((err: Error | null, result?: any) => {
+    if (err) {
+      throw err
+    }
+    res.json(result)
+  })
 })
 
 app.listen(port, (err: Error) => {
