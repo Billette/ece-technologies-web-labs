810fad19b99c8c1ea376f1b3ab04a917e81da70c
Sarah Lehuby <sarah.lehuby@bbox.fr>
2019-11-23 16:40:36 +0100

    Résolution du bug d'importation des fichiers jquery et css

--- lab2/lab2_typescript/dist/server.js
+++ lab2/lab2_typescript/dist/server.js
@@ -5,7 +5,7 @@ var metrics_1 = require("./metrics");
 var app = express();
 var port = process.env.PORT || '8080';
 var path = require("path");
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.set('views', __dirname + "/view");
 app.set('view engine', 'ejs');
 app.get('/', function (req, res) {
--- lab2/lab2_typescript/src/server.ts
+++ lab2/lab2_typescript/src/server.ts
@@ -5,7 +5,7 @@ const app = express()
 const port: string = process.env.PORT || '8080'
 
 import path = require('path')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, '../public')))
 
 app.set('views', __dirname + "/view")
 app.set('view engine', 'ejs');
