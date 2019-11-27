25e40d9cb42f7466893965b270e96af9013c8ef3
Sarah Lehuby <sarah.lehuby@bbox.fr>
2019-11-27 11:50:17 +0100

    lab3, on a put, getAll, getOne, manque delete

--- lab3/src/metrics.ts
+++ lab3/src/metrics.ts
@@ -1,3 +1,7 @@
+import {LevelDB} from './leveldb'
+import WriteStream from 'level-ws'
+
+
 export class Metric {
   public timestamp: string
   public value: number
@@ -9,11 +13,90 @@ export class Metric {
 }
 
 export class MetricsHandler {
-  static get(callback: (error: Error | null, result?: Metric[]) => void) {
-    const result = [
-      new Metric('2013-11-04 14:00 UTC', 12),
-      new Metric('2013-11-04 14:30 UTC', 15)
-    ]
-    callback(null, result)
-  }
+    private db: any 
+  
+    constructor(dbPath: string) {
+      this.db = LevelDB.open(dbPath)
+    }
+
+    public save (
+      key : number,
+      metrics : Metric[],
+      callback: (error:Error | null) => void
+    ){
+      const stream = WriteStream(this.db)
+      stream.on('error', callback)
+      stream.on('close', callback)
+      metrics.forEach((m: Metric) => {
+        stream.write({ key: `metric:${key}:${m.timestamp}`, value: m.value })
+      })
+      stream.end()
+    }
+
+    public getAll (
+      callback: (error:Error | null, result: any) => void
+    ){
+
+      let metrics: Metric[] = []
+      const rs = this.db.createReadStream()
+        .on('data', function (data) {
+          console.log(data.key, '=', data.value) 
+          const timestamp = data.key.split(':')[2]
+          let metric: Metric = new Metric(timestamp, data.value)
+          metrics.push(metric)
+        })
+        .on('error', function (err) {
+          console.log('Oh my!', err)
+          callback(err ,null)
+        })
+        .on('close', function () {
+          console.log('Stream closed')
+        })
+        .on('end', function () {
+          console.log('Stream ended')
+          callback(null, metrics)
+        })
+    }
+
+
+    public getOne(
+      key : number,
+      callback: (error:Error | null, result:any) => void){
+        let metrics: Metric[] = []
+        const rs = this.db.createReadStream()
+        .on('data', function (data) {
+          //console.log("key :", data.key)
+          const tmp = data.key.split(':')[1]
+          //console.log("test :", tmp)
+          //console.log("key parameters :", key)
+          if(tmp === key){
+            console.log(data.key, '=', data.value) 
+            const timestamp = data.key.split(":")[2]
+            let metric: Metric = new Metric(timestamp,data.value)
+            metrics.push(metric)
+          }
+        })
+        .on('error', function (err) {
+          console.log('Oh my!', err)
+          callback(err ,null)
+        })
+        .on('close', function () {
+          console.log('Stream closed')
+        })
+        .on('end', function () {
+          console.log('Stream ended')
+          callback(null, metrics)
+        })
+
+      }
+
+      public deleteOne(
+        key : number,
+        callback: (error:Error | null, result:any) => void
+        ){
+          console.log("key : ",key)
+          const del = this.db.del(key,callback)
+          //console.log("je sais pas", del)
+          //callback(null, del)
+        }
 }
--- lab3/src/server.ts
+++ lab3/src/server.ts
@@ -1,4 +1,5 @@
 import express = require('express')
+import bodyparser = require('body-parser')
 import { MetricsHandler } from './metrics'
 
 const app = express()
@@ -6,6 +7,8 @@ const port: string = process.env.PORT || '8080'
 
 import path = require('path')
 app.use(express.static(path.join(__dirname, '../public')))
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded())
 
 app.set('views', __dirname + "/view")
 app.set('view engine', 'ejs');
@@ -20,6 +23,48 @@ app.get(
         res.render('hello.ejs', {name: req.params.name})
 })
 
+const dbMet: MetricsHandler = new MetricsHandler('./db/metrics')
+
+app.post('/metrics/:id', (req: any, res: any) => {
+  dbMet.save(req.params.id, req.body, (err: Error | null) => {
+    if (err) throw err
+    res.status(200).send('ok')
+  })
+})
+
+app.get('/metrics/', (req:any, res:any) => {
+  dbMet.getAll( (err: Error | null, result:any) => {
+    if (err) throw err
+    res.status(201).json(result)
+  })
+})
+
+app.get('/metrics/:id', (req:any, res:any) => {
+  dbMet.getOne(req.params.id, (err: Error | null, result:any) => {
+    if (err) throw err
+    res.status(201).json(result)
+  })
+})
+
+app.delete('/metrics/:id', (req:any, res:any) => {
+  dbMet.deleteOne(req.params.id, (err: Error | null, result:any) => {
+    if (err) throw err
+    res.status(201).json(result)
+  })
+})
+
+
+app.listen(port, (err: Error) => {
+  if (err) {
+    throw err
+  }
+  console.log(`server is listening on port ${port}`)
+})
+
+
+
+
+/*
 app.get('/metrics.json', (req: any, res: any) => {
   MetricsHandler.get((err: Error | null, result?: any) => {
     if (err) {
@@ -28,10 +73,4 @@ app.get('/metrics.json', (req: any, res: any) => {
     res.json(result)
   })
 })
-
-app.listen(port, (err: Error) => {
-  if (err) {
-    throw err
-  }
-  console.log(`server is listening on port ${port}`)
-})
+*/
